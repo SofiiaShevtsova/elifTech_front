3758c8b6b1acaa149b2c3ed07aba05840f7c2522
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-06-03 13:32:43 +0300

    change map locations

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,7 @@ const Map = () => {
   return (
     <Center w="100%" h="35%" my="20px">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d274450.6497260677!2d11.596580418800349!3d48.134840853450434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sde!4v1603744450305!5m2!1sen!2sde"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d273450.6497260677!2d10.706580448800849!3d48.134840853450434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sde!4v1603744450305!5m2!1sen!2sde"
         width="90%"
         height="100%"
         frameborder="0"
